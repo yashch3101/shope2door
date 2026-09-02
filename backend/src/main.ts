96ced9be38797d57bf078bfd93d171a669efbfbf
Yash Chaurasia <peterchaurasia@gmail.com>
2026-09-02 14:46:41 +0530

    Fix main.ts issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -145,7 +145,7 @@ async function bootstrap() {
     process.env.PORT || 3000,
   );
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(
     `Shop2Door API running on http://localhost:${port}/api/v1`,
